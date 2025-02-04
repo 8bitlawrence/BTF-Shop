2f89836cfc5567731f560370e972450ff66083d3
the_cookiemonster <lawrencetong1030@gmail.com>
2025-02-04 08:21:07 -0600

    Season 3 shop

---
+++ script.js
@@ -0,0 +1,5 @@
+vcode = Math.floor(Math.random() * 100000000) + 1
+function buybtf() {
+  document.getElementById("instuctions").innerHTML = "Email: lawrencetong1030@gmail.com";
+  document.getElementById("instuctions2").innerHTML = "Email this verification code: " + vcode
+}
